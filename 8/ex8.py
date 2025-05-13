c8bc4081f48a068ce16ae5437dbecae52c0be55c
Meirom-Michal <michalb12@gmail.com>
2025-05-13 17:49:56 +0300

    updated ex8

--- 8/ex8.py
+++ 8/ex8.py
@@ -4,6 +4,14 @@ import time
 screen = turtle.Screen()
 screen.setup(width=1000, height=500)
 
+# orli - you dont need to download all the images manually, just clone the repo by opening a terminal and running:
+# cd cd %USERPROFILE%/Downloads
+# git clone https://github.com/eyalm2000/orli.git
+# (make sure git is installed)
+# then, run
+# cd orli/8
+# python ex8.py
+
 for i in range(9):
     screen.addshape(f'images/steve{i}.gif')
 
@@ -32,10 +40,7 @@ def change_image(turtlenumber):
     else:
         turtlenumber.shape('images/steve0.gif')
         turtlenumber.imagecount = 0
-    if turtlenumber == turtle1:
-        x = turtlenumber.xcor() + 10
-    else:
-        x = turtlenumber.xcor() - 10
+    x = turtlenumber.xcor() + (10 if turtlenumber == turtle1 else -10)  
     turtlenumber.goto(x, turtlenumber.ycor())
     time.sleep(0.02)
 
@@ -46,12 +51,10 @@ while True:
     change_image(turtle2)
     if turtle2.xcor() == 100:
         turtletext.goto(50, 150)
-        turtletext.write("hi! i'm steve!")
+        turtletext.write("hi! i'm steve!", font=("Arial", 16))
         turtletext.color('blue')
         time.sleep(3)
         turtletext.hideturtle()
         turtletext.goto(-200, 150)
-        turtletext.write("hi! i'm steve too! that's cool!")
+        turtletext.write("hi! I'm steve too! that's cool!", font=("Arial", 16))
         turtle.done()
-        time.sleep(20)
-        break
