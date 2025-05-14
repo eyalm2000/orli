e3cab388f850da1fbde2ba1151379a8a69188362
Eyal Meirom <eyal.meirom@gmail.com>
2025-05-14 09:21:57 +0300

    new file:   project/media/replace.gif 	modified:   project/part1.py 	new file:   project/part2.py

--- project/part2.py
+++ project/part2.py
@@ -7,21 +7,33 @@ screen.setup(width=1000, height=500)
 screen.addshape('media/paper.gif')
 screen.addshape('media/rock.gif')
 screen.addshape('media/scissors.gif')
+screen.addshape('media/replace.gif')
 
 turtle1 = turtle.Turtle()
 turtle2 = turtle.Turtle()
+replace = turtle.Turtle()
 
 turtle1.penup()
 turtle2.penup()
+replace.penup()
+
+replace.goto(0, -200)
+replace.shape('media/replace.gif')
 
 turtle1.goto(-200, 0)
 turtle2.goto(200, 0)
 
-def choose_shape(turtlenumber): 
+def choose_shape(turtlenumber):
     turtlenumber.shapename = (random.choice(['media/paper.gif', 'media/rock.gif', 'media/scissors.gif']))
     turtlenumber.shape(turtlenumber.shapename)
 
+def button_click(x, y):
+        choose_shape(turtle1)
+        choose_shape(turtle2)
+
 choose_shape(turtle1)
 choose_shape(turtle2)
+
+replace.onclick(button_click)
 
 turtle.done()
